4139bd304a04bc1d163e77706527bc203009a58e
Ibirn <kmcricciuti@gmail.com>
2020-10-27 16:20:37 -0700

    Drier code, better buttons

--- react-front-end/src/components/DialogueScene.jsx
+++ react-front-end/src/components/DialogueScene.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DialogueBox from "./DialogueBox";
+import axios from "axios";
 
 const DialogueScene = (props) => {
   // console.log(props.match.params.id); //pull scene id
+
+  const [dialogues, setDialogues] = useState(["Message"]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(`/api/scene/${props.match.params.id}`) // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        setDialogues(response.data);
+      });
+  }, [index]);
+
   return (
     <div>
       <h1>This is a scene with dialogue</h1>
-      {/* <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vel
-        ratione, et totam blanditiis temporibus perferendis nemo cum aliquam
-        debitis minima eos reiciendis corporis aliquid nihil animi sequi vero!
-        Amet.
-      </p> */}
-      <DialogueBox scene={props.match.params.id} />
-      {/* <Link to="/scene/2">Go</Link> */}
+      <DialogueBox
+        scene={props.match.params.id}
+        dialogue={dialogues[index].story}
+        length={dialogues.length}
+        setIndex={setIndex}
+        index={index}
+      />
     </div>
   );
 };
