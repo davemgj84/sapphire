599af6e8717449c51f595efb4a196ebf5700e54f
Ibirn <kmcricciuti@gmail.com>
2020-10-27 08:40:37 -0700

    Updated queries

--- express-back-end/database/queries.js
+++ express-back-end/database/queries.js
@@ -8,57 +8,35 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
-const getUserWithEmail = function (email) {
-  return pool
-    .query(
-      `
-  SELECT * FROM users 
-  WHERE email = $1`,
-      [`${email}`]
-    )
+const getUserWithEmail = (email) => {
+  const sql = `SELECT * FROM users WHERE email = $1`;
+  const query = pool.query(sql, [email]);
+
+  return query
     .then((res) => {
       return res.rows[0];
     })
     .catch((err) => err);
 };
 
-//needs user-id?
-//catch on parents
-const getDialogueBySceneID = function (sceneID) {
-  const sql = ` SELECT * FROM dialogues WHERE scene_id = $1`;
+const getDialogueBySceneID = (sceneID) => {
+  const sql = `SELECT * FROM dialogues WHERE scene_id = $1`;
   const query = pool.query(sql, [sceneID]);
 
   return query
     .then((res) => {
-      console.log(res.rows[0]);
       return res.rows[0];
     })
     .catch((err) => {
-      console.log(err.stack);
       throw `${err.code} executing fetch`;
     });
-
-  // return pool
-  //   .query(
-  //     `
-  // SELECT * FROM dialogue
-  // WHERE scene_id = $1`,
-  //     [`${sceneID}`]
-  //   )
-  //   .then((res) => {
-  //     return res.rows[0];
-  //   });
-  // .catch((err) => err);
 };
 
-const getBadgesbyUserID = function (userID) {
-  return pool
-    .query(
-      `
-  SELECT * FROM badges
-  WHERE user_id = $1`,
-      [`${userID}`]
-    )
+const getBadgesbyUserID = (userID) => {
+  const sql = `SELECT * FROM badges WHERE user_id = $1`;
+  const query = pool.query(sql, [userID]);
+
+  return query
     .then((res) => {
       return res.rows[0];
     })
@@ -66,15 +44,12 @@ const getBadgesbyUserID = function (userID) {
 };
 
 const getsAdventurebyUserID = (userID) => {
-  return pool
-    .query(
-      `
-  SELECT * FROM adventures
-  WHERE user_id =$1`,
-      [`${userID}`]
-    )
+  const sql = `SELECT * FROM adventures WHERE user_id = $1`;
+  const query = pool.query(sql, [userID]);
+
+  return query
     .then((res) => {
-      return res.row[0];
+      return res.rows[0];
     })
     .catch((err) => err);
 };
