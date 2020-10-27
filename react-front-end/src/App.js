2f43904f947a70cb0e6485b02d6f373ff5320adb
Ibirn <kmcricciuti@gmail.com>
2020-10-27 10:06:26 -0700

    Add front-end fetch

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Content from "./components/Content";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 const App = () => {
+  const [state, setState] = useState("Message");
+
+  const fetchData = () => {
+    return axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log("THIS: ", response); // The entire response from the Rails API
+
+        // console.log(response); // Just the message
+        setState(response.data);
+      });
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Route path="/scene/:id" component={Content} />
       </div>
+      <h1>{state}</h1>
+      <button onClick={() => fetchData()}>Fetch Data</button>
     </BrowserRouter>
   );
 };
