f02a62f6d0d21101a2d82ce6cd34e0158c3be875
Ibirn <kmcricciuti@gmail.com>
2020-10-29 10:43:29 -0700

    Add styling to keep buttons in sight on click through and dynamic bg change in scenes.

--- react-front-end/src/components/DialogueBox.jsx
+++ react-front-end/src/components/DialogueBox.jsx
@@ -3,31 +3,51 @@ import { Link } from "react-router-dom";
 import "../styles/DialogueBox.scss";
 
 export default function DialogueBox(props) {
-  const endLength = (maxLength, currentIndex) => {
-    if (maxLength - 1 === currentIndex) {
+  const endScene = () => {
+    if (props.dialogue.next_dialogue_id === null) {
+      console.log("HELLO");
       return true;
     }
   };
 
-  return (
-    <div className="dialogue-box">
-      {/* <p>{props.dialogue}</p>
-
-      {endLength(props.length, props.index) ? (
-        <Link
-          to={`/scene/${Number(props.scene) + 1}`}
-          className="dialogue-buttons"
-        >
-          <button>Return to Map</button>
-        </Link>
-      ) : (
-        <button
-          className="dialogue-buttons"
-          // onClick={() => props.setIndex((prev) => prev + 1)}
-        >
-          Next
-        </button>
-      )} */}
-    </div>
-  );
+  //renders multiple choices if props.dialogue is an array
+  if (props.dialogue.length) {
+    return (
+      <div className="dialogue-box">
+        <p>{props.dialogue[0].story}</p>
+        <div>
+          {props.dialogue.map((choice) => (
+            <button
+              className="dialogue-buttons"
+              onClick={() => props.current(props.next(choice))}
+            >
+              {choice.label}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    //renders single path
+    return (
+      <div className="dialogue-box">
+        <p>{props.dialogue.story}</p>
+        {endScene() ? (
+          <Link
+            to={`/scene/${Number(props.scene) + 1}`}
+            className="dialogue-buttons"
+          >
+            <button>Return to Map</button>
+          </Link>
+        ) : (
+          <button
+            className="dialogue-buttons"
+            onClick={() => props.current(props.next(props.dialogue))}
+          >
+            {props.dialogue.label}
+          </button>
+        )}
+      </div>
+    );
+  }
 }
