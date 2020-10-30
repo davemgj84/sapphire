9dd9f43be9d121b7f2b23fb7ccbeea41b6846ae4
Ibirn <kmcricciuti@gmail.com>
2020-10-30 10:59:02 -0700

    Edit to pass down props

--- react-front-end/src/components/ShiftingSands.jsx
+++ react-front-end/src/components/ShiftingSands.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react";
 import SandIcon from "./SandIcon";
 // import useMinigameHook from "../hooks/useMinigameHooks";
+import "../styles/sand.scss";
 
 export default function ShiftingSands(props) {
   useEffect(() => {
     window.addEventListener("keyup", props.minigame); //add listener with function
-    return () => window.removeEventListener("keyup", props.minigame); //remove to prevent exponential fuckups
+    return () => {
+      window.removeEventListener("keyup", props.minigame);
+    }; //remove to prevent exponential fuckups
   }, [props.minigame]);
   console.log("Sandporps: ", props);
 
   return (
-    <div>
+    <div className="sand-game">
       {props.pattern.map((a, b) => (
         <SandIcon direction={a} key={b} id={b} pressed={props.pressed[b]} />
       ))}
