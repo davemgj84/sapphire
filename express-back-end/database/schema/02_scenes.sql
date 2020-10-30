ef98d1e404262affc4de0c2017a50106748a2ae1
David <davemgj@gmail.com>
2020-10-30 09:37:03 -0700

    fix merge

--- express-back-end/database/schema/02_scenes.sql
+++ express-back-end/database/schema/02_scenes.sql
@@ -4,9 +4,6 @@ CREATE TABLE scenes (
   id SERIAL PRIMARY KEY NOT NULL,
   -- user_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
-<<<<<<< HEAD
-=======
   -- initial_dialogue INTEGER REFERENCES dialogues(id)
->>>>>>> 5b4274af00c273d394032b831eafabb7b91d01bf
   initial_dialogue INTEGER
 );
