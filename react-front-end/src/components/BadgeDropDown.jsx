f818733b1d710da826c78811ce91f47868d1287a
David <davemgj@gmail.com>
2020-10-31 17:19:37 -0700

    scaled back the drop down to remove animations

--- react-front-end/src/components/BadgeDropDown.jsx
+++ react-front-end/src/components/BadgeDropDown.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { CSSTransition } from "react-transition-group";
 import "../styles/Frame.scss";
 
 const BadgeDropDown = () => {
-  const [activeMenu, setActiveMenu] = useState("main");
-  const [menuHeight, setMenuHeight] = useState(null);
+  // const [activeMenu, setActiveMenu] = useState("main");
   const [badgeTitle, setBadgeTitle] = useState("");
   const [badgeImage, setBadgeImage] = useState("");
   const [badgeDescription, setBadgeDescription] = useState("");
-
-  const calcHeight = (el) => {
-    const height = el.offSetHeight;
-    setMenuHeight(height);
-  };
 
   useEffect(() => {
     axios.get(`/api/badges/1`).then((response) => {
@@ -25,53 +18,18 @@ const BadgeDropDown = () => {
     });
   }, []);
 
-  const BadgeDropDown = (props) => {
-    return (
-      <a
-        href="#"
-        className="badge-item"
-        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
-      >
-        {activeMenu !== "main" && (
-          <span className="back-icon-button">{props.leftIcon}</span>
-        )}
-        <span className="badge-name">{props.children}</span>
-      </a>
-    );
-  };
-
   return (
-    <div className="dropdown" style={{ height: menuHeight }}>
-      <CSSTransition
-        in={activeMenu === "main"}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-primary"
-        onEnter={calcHeight}
-      >
-        <div className="menu">
-          <BadgeDropDown leftIcon={null} goToMenu="description">
-            <img className="badge-icon" src={badgeImage} alt="badge icon" />
-            {badgeTitle}
-          </BadgeDropDown>
-        </div>
-      </CSSTransition>
-
-      <CSSTransition
-        in={activeMenu === "description"}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-secondary"
-      >
-        <div className="menu">
-          <BadgeDropDown
-            leftIcon={<i class="fas fa-arrow-left"></i>}
-            goToMenu="main"
+    <div className="dropdown">
+      <div className="menu">
+        <a href="#" className="badge-item">
+          <img
+            className="badge-icon"
+            src="https://bit.ly/3jOKHXM"
+            alt="badge icon"
           />
-
-          <p className="badge-description">{badgeDescription}</p>
-        </div>
-      </CSSTransition>
+          <span className="badge-name">{badgeTitle}</span>
+        </a>
+      </div>
     </div>
   );
 };
