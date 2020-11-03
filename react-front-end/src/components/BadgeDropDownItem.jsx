9a825bbeeec3e151cb037bcfc1009c9a7cd1a47a
David <davemgj@gmail.com>
2020-11-03 10:37:24 -0800

    tidy drop down and changed font

--- react-front-end/src/components/BadgeDropDownItem.jsx
+++ react-front-end/src/components/BadgeDropDownItem.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const BadgeDropDownItem = (props) => {
   return (
     <div className="badge-component">
-      <span className="badge-item">
+      <div className="badge-item">
         <img className="badge-icon" src={props.image} alt="badge icon" />
-      </span>
-      <span className="name-and-description">
-        <p className="badge-name">{props.title}</p>
-        <p className="badge-description">{props.description}</p>
-      </span>
+      </div>
+      <div className="align-left-please">
+        <div className="name-and-description">
+          <p className="badge-name">{props.title}</p>
+          <p className="badge-description">{props.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
